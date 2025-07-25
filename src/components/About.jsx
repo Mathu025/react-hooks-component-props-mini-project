ac83615646699d575f4c8e65675e6f48d9b3f8ea
Mathu025 <mathualex72@gmail.com>
2025-07-25 22:48:29 +0300

    Add complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = ( {image="https://via.placeholder.com/215", about}) => {
   return (
    <aside>
-    <img src={image} alt="blog image" />
+    <img src={image} alt="blog logo" />
     <p>{about}</p>
    </aside>
   )
